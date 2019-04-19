a2898cc5cc42a7351ff091f5665ff1057916ac80
码农 <lyong155@163.com>
2019-04-19 13:28:10 +0800

    关联真实姓名到表tinfo的extends1字段

--- src/main/java/lzt/xiaodai/cn/controller/step/StepThreeController.java
+++ src/main/java/lzt/xiaodai/cn/controller/step/StepThreeController.java
@@ -3,12 +3,10 @@ package lzt.xiaodai.cn.controller.step;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lzt.xiaodai.cn.common.ResultInfo;
 import lzt.xiaodai.cn.entity.TContact;
+import lzt.xiaodai.cn.entity.TIdentity;
 import lzt.xiaodai.cn.entity.TInfo;
 import lzt.xiaodai.cn.entity.TProject;
-import lzt.xiaodai.cn.service.TContactService;
-import lzt.xiaodai.cn.service.TInfoService;
-import lzt.xiaodai.cn.service.TPhaseService;
-import lzt.xiaodai.cn.service.TProjectService;
+import lzt.xiaodai.cn.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,6 +28,8 @@ public class StepThreeController {
     TPhaseService tPhaseService;
     @Autowired
     TContactService tContactService;
+    @Autowired
+    TIdentityService tIdentityService;
     @PostMapping("/put/step3")
     public ResultInfo handlerStep3(@RequestParam String mobile, @RequestBody TInfo tInfo){
         tInfoService.save(tInfo);
@@ -41,7 +41,7 @@ public class StepThreeController {
             tContactService.saveBatch(contacts);
         }
 
-
+        Integer identityId = null;
         TInfo one = tInfoService.getOne(new QueryWrapper<>(tInfo));
         QueryWrapper<TProject> condition = new QueryWrapper<>();
         condition.eq("mobile",mobile);
@@ -49,7 +49,18 @@ public class StepThreeController {
         for (TProject project : list){
             project.setInfoid(one.getId());
             project.setPhaseid(3);
+            if (identityId == null){
+                identityId = project.getIdentityid();
+            }
             tProjectService.updateById(project);
+        }
+        //关联真实姓名到tinfo表字段extends1
+        if (identityId != null){
+            TIdentity ide = tIdentityService.getById(identityId);
+            if (ide != null){
+                tInfo.setExtends1(ide.getRealname());
+            }
+            tInfoService.updateById(tInfo);
         }
         return ResultInfo.ok(tProjectService.gettProjectVos(list));
     }
